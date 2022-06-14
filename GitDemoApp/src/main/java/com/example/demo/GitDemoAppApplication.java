9a0e5ef9f163fbaa57b8d50356ca02cf90b6a783
pendurthi.ritesh <pendurthi.ritesh@HYDPCM374297L.ad.infosys.com>
2022-06-14 15:41:31 +0530

    hellooo

--- GitDemoApp/src/main/java/com/example/demo/GitDemoAppApplication.java
+++ GitDemoApp/src/main/java/com/example/demo/GitDemoAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoAppApplication {
 
 	public static void main(String[] args) {
-		System.out.println("welcome");
+		System.out.println("welcomes");
 		SpringApplication.run(GitDemoAppApplication.class, args);
 	}
 
